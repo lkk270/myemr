fa45a4ebd93a7c5bd54f086f308b8c7243fbf843
Lee Korff-Korn <leekk270@gmail.com>
2023-10-13 13:21:44 +0200

    added linter

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,24 +9,20 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Emridoc",
-  description: "A patient focused EMR",
+	title: "Emridoc",
+	description: "A patient focused EMR",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body className={cn("bg-secondary", font.className)}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<ClerkProvider>
+			<html lang="en" suppressHydrationWarning>
+				<body className={cn("bg-secondary", font.className)}>
+					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	);
 }

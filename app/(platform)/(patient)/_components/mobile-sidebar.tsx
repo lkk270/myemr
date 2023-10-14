dfaa60db97e7a52c8cc9288be475396ba53bbd8b
Lee Korff-Korn <leekk270@gmail.com>
2023-10-14 21:44:38 +0200

    patient nav bar, reorg of folders

---
+++ app/(platform)/(patient)/_components/mobile-sidebar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sidebar } from "./sidebar";
+import { Menu } from "lucide-react";
+
+export const MobileSidebar = () => {
+	const triggerClassName = "mr-2 sm:hidden";
+	return (
+		<Sheet>
+			<SheetTrigger className={triggerClassName}>
+				<Menu />
+			</SheetTrigger>
+			<SheetContent side="left" className="w-32 p-0 pt-10 bg-secondary">
+				<Sidebar />
+			</SheetContent>
+		</Sheet>
+	);
+};

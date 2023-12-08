280e79ed09d8a6c700e54572eb2457fb4b87641c
Lee Korff-Korn <leekk270@gmail.com>
2023-12-08 21:20:22 +0100

    added table components

--- app/(platform)/(patient)/(non-file-system)/medications/page.tsx
+++ app/(platform)/(patient)/(non-file-system)/medications/page.tsx
@@ -1,9 +1,13 @@
 import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
-import { Demographics } from "../../_components/demographics";
 
 import { decryptKey, decryptMultiplePatientFields } from "@/lib/encryption";
+
+import Image from "next/image";
+
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
 
 const PatientMedications = async () => {
   const { userId } = auth();
@@ -11,33 +15,39 @@ const PatientMedications = async () => {
   if (!userId) {
     return redirectToSignIn;
   }
-  // const patientDemographics = await prismadb.patientProfile.findUnique({
-  //   where: {
-  //     userId: userId,
-  //   },
-  //   select: {
-  //     imageUrl: true,
-  //     email: true,
-  //     firstName: true,
-  //     lastName: true,
-  //     gender: true,
-  //     dateOfBirth: true,
-  //     maritalStatus: true,
-  //     race: true,
-  //     mobilePhone: true,
-  //     homePhone: true,
-  //     height: true,
-  //     weight: true,
-  //     unit: true,
-  //     insuranceProvider: true,
-  //     policyNumber: true,
-  //     groupNumber: true,
-  //     addresses: true,
-  //     symmetricKey: true,
-  //   },
-  // });
+  const patientMedications = await prismadb.patientProfile.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      medications: true,
+      symmetricKey: true,
+    },
+  });
 
-  return <div className="flex pt-10 px-10 h-full justify-center">Hello</div>;
+  if (!patientMedications) {
+    return <div>something went wrong</div>;
+  }
+  let decryptedPatientMedications;
+  try {
+    const decryptedSymmetricKey = decryptKey(patientMedications.symmetricKey, "patientSymmetricKey");
+    decryptedPatientMedications = decryptMultiplePatientFields(patientMedications, decryptedSymmetricKey);
+  } catch (e) {
+    return <div>something went wrong</div>;
+  }
+  console.log(decryptedPatientMedications);
+  return (
+    <div className="flex pt-10 sm:px-10 h-full justify-center">
+      <div className="h-full flex-1 flex-col space-y-8 p-2 sm:p-8 flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Medications</h2>
+          </div>
+        </div>
+        <DataTable data={decryptedPatientMedications} columns={columns} />
+      </div>
+    </div>
+  );
 };
 
 export default PatientMedications;

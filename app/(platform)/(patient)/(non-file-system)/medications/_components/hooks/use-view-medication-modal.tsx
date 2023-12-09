9ff378a53cce61f202bc187851e854db6619a759
Lee Korff-Korn <leekk270@gmail.com>
2023-12-09 20:41:23 +0100

    added isEdit to modal

--- app/(platform)/(patient)/(non-file-system)/medications/_components/hooks/use-view-medication-modal.tsx
+++ app/(platform)/(patient)/(non-file-system)/medications/_components/hooks/use-view-medication-modal.tsx
@@ -3,14 +3,16 @@ import { Medication } from "@prisma/client";
 
 interface useViewMedicationStore {
   isOpen: boolean;
+  isEdit: boolean;
   medication: Medication | null;
-  onOpen: (medication: Medication) => void;
+  onOpen: (medication: Medication, isEdit: boolean) => void;
   onClose: () => void;
 }
 
 export const useViewMedicationModal = create<useViewMedicationStore>((set) => ({
   isOpen: false,
   medication: null,
-  onOpen: (medication) => set({ isOpen: true, medication: medication }),
+  isEdit: false,
+  onOpen: (medication, isEdit) => set({ isOpen: true, medication: medication, isEdit: isEdit }),
   onClose: () => set({ isOpen: false }),
 }));

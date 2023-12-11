5d8c7ece5f93655b075e2f2180f483b10d797e2f
Lee Korff-Korn <leekk270@gmail.com>
2023-12-11 14:46:37 +0100

    added type

--- app/(platform)/(patient)/(non-file-system)/medications/_components/table/custom-data-table-row-actions.tsx
+++ app/(platform)/(patient)/(non-file-system)/medications/_components/table/custom-data-table-row-actions.tsx
@@ -1,12 +1,8 @@
 import { DataTableRowActions } from "@/components/table/data-table-row-actions";
 import { useViewMedicationModal } from "../hooks/use-view-medication-modal";
-import { Row } from "@tanstack/react-table";
+import { DataTableRowActionsProps } from "@/app/types";
 
-interface CustomDataTableRowActionsProps<TData> {
-  row: Row<TData>;
-}
-
-export function CustomDataTableRowActions<TData>({ row }: CustomDataTableRowActionsProps<TData>) {
+export function CustomDataTableRowActions<TData>({ row }: DataTableRowActionsProps<TData>) {
   const { onOpen } = useViewMedicationModal();
   return <DataTableRowActions row={row} onOpen={onOpen} />;
 }

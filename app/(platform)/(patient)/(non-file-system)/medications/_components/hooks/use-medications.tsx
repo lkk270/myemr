eeaee7a9f21cab69fc3e3b35540078200176ffca
Lee Korff-Korn <leekk270@gmail.com>
2023-12-12 20:50:26 +0100

    add validation for routes medication edit dosagehistory works

--- app/(platform)/(patient)/(non-file-system)/medications/_components/hooks/use-medications.tsx
+++ app/(platform)/(patient)/(non-file-system)/medications/_components/hooks/use-medications.tsx
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 import { MedicationType } from "@/app/types";
-
+import { DosageHistory } from "@prisma/client";
 interface MedicationStore {
   medications: MedicationType[];
   medicationsSet: boolean; // New field to track if medications have been set
   setMedications: (medications: MedicationType[]) => void;
   addMedication: (medication: MedicationType) => void;
-  updateMedication: (updatedMedication: MedicationType) => void;
+  updateMedication: (updatedMedication: MedicationType, newDosageHistory?: DosageHistory) => void;
   deleteMedication: (medicationId: string) => void;
   isMedicationNameExist: (name: string) => boolean;
 }
@@ -16,11 +16,19 @@ export const useMedicationStore = create<MedicationStore>((set, get) => ({
   medicationsSet: false, // Initial value is false
   setMedications: (medications) => set({ medications, medicationsSet: true }),
   addMedication: (medication) => set((state) => ({ medications: [...state.medications, medication] })),
-  updateMedication: (updatedMedication) =>
+  updateMedication: (updatedMedication, newDosageHistory) =>
     set((state) => ({
-      medications: state.medications.map((medication) =>
-        medication.id === updatedMedication.id ? updatedMedication : medication,
-      ),
+      medications: state.medications.map((medication) => {
+        if (medication.id === updatedMedication.id) {
+          return {
+            ...updatedMedication,
+            dosageHistory: newDosageHistory
+              ? [...(medication.dosageHistory || []), newDosageHistory]
+              : medication.dosageHistory,
+          };
+        }
+        return medication;
+      }),
     })),
   deleteMedication: (medicationId) =>
     set((state) => ({

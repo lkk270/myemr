af0fc0056c618cbc36af589595ebb669cc1dcbe2
Lee Korff-Korn <leekk270@gmail.com>
2023-12-14 03:59:07 +0100

    dnd working

--- app/(platform)/(patient)/(file-system)/(test)/fill-flex-parent.tsx
+++ app/(platform)/(patient)/(file-system)/(test)/fill-flex-parent.tsx
@@ -5,22 +5,21 @@ import mergeRefs from "./merge-refs";
 import useResizeObserver from "use-resize-observer";
 
 type Props = {
+  realWidth: number;
+  height: number;
   children: (dimens: { width: number; height: number }) => ReactElement;
-};
-
-const style = {
-  flex: 1,
-  width: "100%",
-  height: "100%",
-  minHeight: 0,
-  minWidth: 0,
 };
 
 export const FillFlexParent = React.forwardRef(function FillFlexParent(props: Props, forwardRef) {
   const { ref, width, height } = useResizeObserver();
+
+  // Use the realWidth from the props instead of the observed width.
+  const effectiveWidth = props.realWidth;
+
   return (
-    <div style={style} ref={mergeRefs(ref, forwardRef)}>
-      {width && height ? props.children({ width, height }) : null}
+    <div className="flex-1 w-full h-full min-h-[0px] min-w-[0px]" ref={mergeRefs(ref, forwardRef)}>
+      {/* Use the effectiveWidth for the width */}
+      {props.children({ width: effectiveWidth, height: props.height })}
     </div>
   );
 });

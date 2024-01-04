6ed4989c918263aef07c2049397720f2803fb3b4
Lee Korff-Korn <leekk270@gmail.com>
2024-01-04 18:55:30 +0100

    fixed css issue for good

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: {
-    "postcss-preset-env": {
-      stage: 1, // or another stage that suits your needs
-    },
+    "postcss-nesting": {}, // Correct syntax for plugin inclusion
     tailwindcss: {},
     autoprefixer: {},
   },

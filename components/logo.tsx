b0409aa0950e6331bee99e1195ca213d6f729795
Lee Korff-Korn <leekk270@gmail.com>
2024-01-04 19:01:37 +0100

    minor fix

--- components/logo.tsx
+++ components/logo.tsx
@@ -21,7 +21,7 @@ export const Logo = ({ textColor, showText = true }: LogoProps) => {
   return (
     <Link href="/" className={cn(showText ? "sm:w-32" : "")}>
       <div className="flex items-center gap-x-2">
-        <Image src="/logo.svg" height="40" width="40" alt="Logo" draggable={false} />
+        <Image priority={true} src="/logo.svg" height="40" width="40" alt="Logo" draggable={false} />
         {showText && (
           <p className={cn("font-semibold text-sm sm:flex sm:text-lg", font.className, textColor && `text-primary/70`)}>
             Emridoc

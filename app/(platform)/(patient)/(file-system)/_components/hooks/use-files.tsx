efe71a02825e7ac6b1b537e5a76c552f4c7a56e6
Lee Korff-Korn <leekk270@gmail.com>
2024-01-09 17:27:46 +0100

    style fixes

---
+++ app/(platform)/(patient)/(file-system)/_components/hooks/use-files.tsx
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { MedicationType } from "@/app/types";
+import { DosageHistory } from "@prisma/client";
+interface FileStore {
+  medications: MedicationType[];
+  medicationsSet: boolean; // New field to track if medications have been set
+  setMedications: (medications: MedicationType[]) => void;
+  addMedication: (medication: MedicationType) => void;
+  updateMedication: (updatedMedication: MedicationType, newDosageHistory?: DosageHistory) => void;
+  deleteMedication: (medicationId: string) => void;
+  isMedicationNameExist: (name: string) => boolean;
+}
+
+export const useFileStore = create<FileStore>((set, get) => ({
+  medications: [],
+  medicationsSet: false, // Initial value is false
+  setMedications: (medications) => set({ medications, medicationsSet: true }),
+  addMedication: (medication) => set((state) => ({ medications: [...state.medications, medication] })),
+  updateMedication: (updatedMedication, newDosageHistory) =>
+    set((state) => ({
+      medications: state.medications.map((medication) => {
+        if (medication.id === updatedMedication.id) {
+          return {
+            ...updatedMedication,
+            dosageHistory: newDosageHistory
+              ? [...(medication.dosageHistory || []), newDosageHistory]
+              : medication.dosageHistory,
+          };
+        }
+        return medication;
+      }),
+    })),
+  deleteMedication: (medicationId) =>
+    set((state) => ({
+      medications: state.medications.filter((medication) => medication.id !== medicationId),
+    })),
+  isMedicationNameExist: (name) => {
+    const state = get(); // Correctly use 'get' to access the state
+    return state.medications.some((medication) => medication.name === name);
+  },
+}));
--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -108,7 +108,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "cursor-pointer relative flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "cursor-pointer relative flex select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-primary/5 aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className,
     )}
     {...props}

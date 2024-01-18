1bb9494f1a68f8820003cba08022e12deff30d62
Lee Korff-Korn <leekk270@gmail.com>
2024-01-19 00:32:47 +0100

    fixes to navbar, added folder action btns

--- app/(platform)/(patient)/(file-system)/_components/navbar.tsx
+++ app/(platform)/(patient)/(file-system)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { GenericNavigationMenu } from "@/components/generic-navigation-menu";
 import { navRoutes } from "@/lib/constants";
 import { SearchBox } from "./search-box";
 import { UserButton } from "@/auth/components/auth/user-button";
+import { useMediaQuery } from "usehooks-ts";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -16,19 +17,30 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+  const isMobile = useMediaQuery("(max-width: 450px)");
+
   return (
     <>
       <nav className="justify-between px-3 py-2 w-full flex items-center gap-x-4">
         {isCollapsed && (
-          <div className="flex items-center gap-x-2">
+          <div className="flex items-center gap-x-3">
             <Logo showText={false} />
             <Folders role="button" onClick={onResetWidth} className="w-6 h-6" />
+            <SearchBox />
+          </div>
+        )}
+        {!isCollapsed && (
+          <div className="flex items-center px-3">
+            <SearchBox />
           </div>
         )}
 
-        <div className="flex items-center">
-          <SearchBox />
-        </div>
+        {/* {!isMobile && (
+          <div className="flex items-center">
+            <SearchBox />
+          </div>
+        )} */}
+        {/* <div className="flex-grow"></div> */}
         <div className="flex items-center gap-x-2">
           <GenericNavigationMenu navRoutes={navRoutes} />
           <ModeToggle />

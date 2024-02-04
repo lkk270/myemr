fdd733ca823b6cf3e8a8fd3a2b47b59bb97bea3e
Lee Korff-Korn <leekk270@gmail.com>
2024-02-04 23:37:49 +0100

    minor fix

--- app/(platform)/(patient)/(file-system)/(routes)/files/page.tsx
+++ app/(platform)/(patient)/(file-system)/(routes)/files/page.tsx
@@ -3,7 +3,7 @@ import { RootNodePageHeader } from "../../_components/root-node-page-header";
 const RootFileSystemPage = async () => {
   return (
     <div className="pt-16 px-6">
-      <div className="text-lg font-bold py-3">Home</div>
+      <div className="text-2xl font-extrabold py-3 underline">Home</div>
       <RootNodePageHeader />
     </div>
   );

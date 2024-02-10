1e4cc6b4cbf3cff1de5347fddbc065ec8c1c44c7
Lee Korff-Korn <leekk270@gmail.com>
2024-02-10 18:41:45 +0100

    access code working

--- app/(platform)/(patient)/(non-file-system)/patient-home/_components/patient-home.tsx
+++ app/(platform)/(patient)/(non-file-system)/patient-home/_components/patient-home.tsx
@@ -3,34 +3,31 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Separator } from "@radix-ui/react-separator";
+import { useMediaQuery } from "usehooks-ts";
+import { cn } from "@/lib/utils";
+import { GenerateCode } from "./generate-code";
 
 export const PatientHome = () => {
+  const isMobile = useMediaQuery("(max-width:640px)");
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2">
-      <div className="bg-primary/1 grid grid-rows-2 w-full py-12 lg:h-[calc(100vh-64px)]">
-        <div className="container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
-          <div className="space-y-3">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Generate a Code</h2>
-            <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              Click the button below to generate a unique code for access.
-            </p>
-          </div>
-          <div className="mx-auto w-full max-w-sm space-y-4">
-            <Button size="lg">Generate Code</Button>
-          </div>
-        </div>
-        <div className="container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
-          <Separator className="h-[1px] bg-primary/20" />
-
+      <div className="grid grid-rows-2 w-full py-4 px-1 sm:px-4 lg:h-[calc(100vh-64px)] gap-y-4 bg-primary/1">
+        <GenerateCode />
+        <div
+          className={cn(
+            isMobile && "min-w-[90vw]",
+            "shadow-lg container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10 bg-primary/5  dark:bg-[#1a1a1a] rounded-lg",
+          )}
+        >
           <div className="space-y-3">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Add a provider</h2>
             <p className="mx-auto max-w-[600px] text-gray-500 md:text-lg/relaxed lg:text-base/relaxed xl:text-lg/relaxed dark:text-gray-400">
-              Did a provider share a connect code with you? Enter it here so they can be connected to your account. You can
-              always modify their access in{" "}
+              Did a provider share a connect code with you? Enter it here so they can be connected to your account. You
+              can always modify their access in{" "}
               <a className="underline" href="/providers">
                 providers
               </a>
+              .
             </p>
           </div>
           <div className="mx-auto w-full max-w-sm space-y-4">
@@ -38,8 +35,13 @@ export const PatientHome = () => {
           </div>
         </div>
       </div>
-      <div className="bg-primary/5 w-full py-12 lg:h-[calc(100vh-64px)]">
-        <div className="container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
+      <div className="grid grid-rows-1 w-full py-4 px-4 lg:pr-4 lg:pl-0 lg:h-[calc(100vh-64px)] gap-y-4 bg-primary/1">
+        <div
+          className={cn(
+            isMobile && "min-w-[90vw]",
+            "shadow-lg container grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10 bg-primary/5 dark:bg-[#1a1a1a] rounded-lg",
+          )}
+        >
           <div className="space-y-3">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Request Your Records</h2>
             <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">

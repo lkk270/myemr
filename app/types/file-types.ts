635585e781c3c1d2637f7115913c8188a752487a
Lee Korff-Korn <leekk270@gmail.com>
2024-02-20 21:15:42 +0100

    fixed abort issue

--- app/types/file-types.ts
+++ app/types/file-types.ts
@@ -85,7 +85,7 @@ export type MenuItemData = {
 
 export type FileWithStatus = {
   file: File;
-  status?: "waiting" | "uploading" | "uploaded" | "error" | "canceled" | null;
+  status?: "waiting" | "uploading" | "uploaded" | "error" | "canceled" | "gotPSU" | null;
   isRetrying?: boolean;
   controller: AbortController;
   insuranceSide?: "front" | "back";

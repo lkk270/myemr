0b6d89bd26759c65afb2bbc5369f2f310a3bde0d
Lee Korff-Korn <leekk270@gmail.com>
2024-02-24 18:32:18 +0100

    moved auth

--- auth/lib/mail.ts
+++ auth/lib/mail.ts
@@ -59,7 +59,7 @@ export const sendRequestRecordsEmail = async (
   },
 ) => {
   // const confirmLink = `${domain}/auth/new-verification?token=${token}`;
-  const requestRecordsLink = `http://localhost:3000/request-records?token=${token}`;
+  const requestRecordsLink = `http://localhost:3000/upload-records?token=${token}`;
   let buffer = null;
   try {
     buffer = await getBuffer({ data: { ...dataForLetter, requestRecordsLink } });
@@ -74,29 +74,13 @@ export const sendRequestRecordsEmail = async (
     to: "leekk270@gmail.com",
     cc: dataForLetter.email,
     subject: "Request for records",
-    attachments: [{ filename: "letter.pdf", content: buffer }],
+    attachments: [
+      {
+        filename: `request_for_records_${dataForLetter.firstName.toLowerCase()}_${dataForLetter.lastName.toLowerCase()}.pdf`,
+        content: buffer,
+      },
+    ],
     react: RequestRecordsEmail({ dataForLetter: dataForLetter, requestRecordsLink: requestRecordsLink }),
-    // html: `
-    // <div style="font-size: 14px; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; color: #333;">
-    //     <div style="text-align: center; margin-bottom: 20px;">
-    //         <img src="${logoBase64Url}" alt="MyEmr Logo" style="width: 100px; height: auto;">
-    //         <h2 style="font-size: 24px; font-weight: bold;">MyEmr</h2>
-    //     </div>
-    //     <div>
-    //     <p>Dear Provider,</p>
-    //     <p>This letter, sent by your patient ${dataForLetter.firstName} ${dataForLetter.lastName} (DOB ${dataForLetter.dateOfBirth}) through their MyEmr account, is a formal request for their medical records. Attached, you will find a notarized document that provides further details and ensures the legitimacy of the request on the patient's behalf. Please save the notarized document in your records.</p>
-    //     <p>Please <a href="${requestRecordsLink}">click here</a> to upload the patient's records. The link is valid for 30 days from the delivery of this email. It's important to ensure that ALL records in your possession for
-    //     the patient in question are uploaded and that they are accurate & up-to-date for compliance and continuity of
-    //     care.</p>
-    //     <p>If you have any questions, require further information, or believe this request is a mistake,
-    //     please do not hesitate to contact me [the patient] at ${dataForLetter.email} or MyEmr at
-    //     support@myemr.io.</p>
-    //     <p>Thank you in advance for your compliance of the patient's request.</p>
-    //     <p>Sincerely,</p>
-    //     <p>The MyEmr Team</p>
-    //     </div>
-    // </div>
-    // `,
   });
   if (response.error) {
     console.log(response);

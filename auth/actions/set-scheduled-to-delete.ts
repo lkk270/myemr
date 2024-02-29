54d06dc47451485eda6f0fb039cb693710b22f40
Lee Korff-Korn <leekk270@gmail.com>
2024-02-29 12:11:14 +0100

    added delete account

---
+++ auth/actions/set-scheduled-to-delete.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import prismadb from "@/lib/prismadb";
+
+export const setScheduledToDelete = async (type: "PATIENT" | "PROVIDER", newValue: boolean, userId: string) => {
+  await prismadb.$transaction(
+    async (prisma) => {
+      if (type === "PATIENT") {
+        await prisma.patientProfile.update({
+          where: {
+            userId: userId,
+          },
+          data: {
+            scheduledToDelete: newValue,
+          },
+        });
+      } else if (type === "PROVIDER") {
+        await prisma.providerProfile.update({
+          where: {
+            userId: userId,
+          },
+          data: {
+            scheduledToDelete: newValue,
+          },
+        });
+      }
+
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          scheduledToDelete: newValue,
+        },
+      });
+    },
+    { timeout: 20000 },
+  );
+};

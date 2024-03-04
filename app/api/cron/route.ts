eada5e226da248935e61da8d71b15f0bda0d4678
Lee Korff-Korn <leekk270@gmail.com>
2024-03-04 12:19:43 +0100

    added restrictfiles cron

---
+++ app/api/cron/route.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { NextRequest, NextResponse } from "next/server";
+import { restrictFilesCron } from "@/lib/cron/restrict-files";
+
+export async function GET(request: NextRequest) {
+  try {
+    const authHeader = request.headers.get("authorization");
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+    await restrictFilesCron(authHeader);
+
+    return new NextResponse("Success", { status: 200 });
+  } catch (error: any) {
+    console.log(error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}

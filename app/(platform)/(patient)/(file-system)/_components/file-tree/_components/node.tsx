b5d55df7d9d61d992a80654f244faf5166e0b556
Lee Korff-Korn <leekk270@gmail.com>
2024-03-11 13:09:40 +0100

    added file schema

--- app/(platform)/(patient)/(file-system)/_components/file-tree/_components/node.tsx
+++ app/(platform)/(patient)/(file-system)/_components/file-tree/_components/node.tsx
@@ -58,6 +58,7 @@ const Node: React.FC<NodeProps> = ({ node, style, dragHandle, tree }) => {
     path: nodeData.path,
     namePath: nodeData.namePath,
     isFile: nodeData.isFile,
+    isRoot: nodeData.isRoot,
     restricted: nodeData.restricted,
   };
 

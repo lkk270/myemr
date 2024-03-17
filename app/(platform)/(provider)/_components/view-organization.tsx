b1dabe65005ad32bc8690990527856239f899e3d
Lee Korff-Korn <leekk270@gmail.com>
2024-03-17 14:02:49 +0100

    fixes

--- app/(platform)/(provider)/_components/view-organization.tsx
+++ app/(platform)/(provider)/_components/view-organization.tsx
@@ -1,35 +1,99 @@
-import { Button } from "@/components/ui/button";
+import { OrganizationWithRoleType } from "@/app/types";
+import { Separator } from "@/components/ui/separator";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
-export const ViewOrganization = ({ company }: any) => {
+// import { AddressAccordion } from "./address-accordion";
+import { cn } from "@/lib/utils";
+interface ViewOrganizationProps {
+  initialData: OrganizationWithRoleType;
+}
+export const ViewOrganization = ({ initialData }: ViewOrganizationProps) => {
   return (
-    <div className="max-w-md mx-auto rounded-xl  overflow-hidden md:max-w-2xl">
-      <div className="md:flex">
-        <div className="md:flex-shrink-0">
-          <img className="h-48 w-full object-cover md:w-48" src={company.image} alt={company.title} />
+    <div className="h-full p-4 w-full max-w-3xl mx-auto">
+      <div className="space-y-4">
+        <div className="space-y-2 w-full">
+          <h3 className="text-lg font-medium">General Information</h3>
+          <p className="text-sm text-muted-foreground">General information about your organization</p>
+          <Separator className="bg-primary/10" />
         </div>
-        <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            {company.organizationType}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div>
+            <strong>Title:</strong>
+            <p>{initialData.title}</p>
           </div>
-          <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
-            {company.title}
-          </a>
-          <p className="mt-2 text-gray-500">{company.subtitle}</p>
-          <p className="mt-2 text-gray-500">{company.description}</p>
-          <div className="mt-4">
-            <div className="text-gray-900 font-semibold">
-              Email:{" "}
-              <a href={`mailto:${company.email}`} className="text-indigo-500 hover:underline">
-                {company.email}
-              </a>
-            </div>
-            <div className="text-gray-900 font-semibold">
-              Phone:{" "}
-              <a href={`tel:${company.phone}`} className="text-indigo-500 hover:underline">
-                {company.phone}
-              </a>
-            </div>
+          <div>
+            <strong>Organization Type:</strong>
+            <p>{initialData.organizationType}</p>
           </div>
+          <div>
+            <strong>Category:</strong>
+            <p>{initialData.category}</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div>
+            <strong>Subtitle:</strong>
+            <p>{initialData.subTitle}</p>
+          </div>
+          <div>
+            <strong>Description:</strong>
+            <p>{initialData.description}</p>
+          </div>
+        </div>
+
+        <div className="space-y-2 w-full">
+          <h3 className="text-lg font-medium">Contact Information</h3>
+          <Separator className="bg-primary/10" />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div>
+            <strong>Main Email:</strong>
+            <p>{initialData.mainEmail}</p>
+          </div>
+          <div>
+            <strong>Main Phone:</strong>
+            <p>{initialData.mainPhone}</p>
+          </div>
+        </div>
+
+        <div className="space-y-2 w-full">
+          <h3 className="text-lg font-medium">Addresses</h3>
+          <Separator className="bg-primary/10" />
+        </div>
+        <div>
+          {initialData?.addresses?.map((address) => (
+            <div className="flex flex-col gap-y-1.5" key={address.id}>
+              <div className="flex flex-row justify-between gap-2 items-center">
+                <Accordion type="multiple" className="space-y-2 w-full">
+                  <AccordionItem className="border-none" value={address.id}>
+                    <AccordionTrigger
+                      className={cn(
+                        "flex items-center gap-x-2 p-1.5 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline",
+                      )}
+                    >
+                      {address.name}
+                    </AccordionTrigger>
+                    <AccordionContent className="pt-1 text-primary/70 text-md">
+                      <div className="p-4 rounded border border-secondary flex items-center gap-4">
+                        <div className="grid gap-2.5">
+                          <p className="font-semibold leading-none">{address.name}</p>
+                          <address className="not-italic leading-none space-y-1.5">
+                            <p>{address.address}</p>
+                            <p>{address.address2}</p>
+                            <p>
+                              {address.city}, {address.state}, {address.zipcode}
+                            </p>
+                          </address>
+                        </div>
+                        {/* <Button size="sm">Edit</Button> */}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </div>
+              <Separator className="bg-primary/10" />
+            </div>
+          ))}
         </div>
       </div>
     </div>

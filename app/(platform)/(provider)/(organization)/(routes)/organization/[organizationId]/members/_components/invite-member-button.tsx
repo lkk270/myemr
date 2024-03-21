cd383022b5f9517ba1c61418f883001f341d9a31
Lee Korff-Korn <leekk270@gmail.com>
2024-03-21 19:04:10 +0100

    minor fix

--- app/(platform)/(provider)/(organization)/(routes)/organization/[organizationId]/members/_components/invite-member-button.tsx
+++ app/(platform)/(provider)/(organization)/(routes)/organization/[organizationId]/members/_components/invite-member-button.tsx
@@ -3,6 +3,8 @@
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { useState } from "react";
 import { InviteMemberForm } from "./invite-member-form";
+import { useCurrentUser } from "@/auth/hooks/use-current-user";
+import { useOrganizationMembersStore } from "../hooks/use-members";
 
 interface InviteMemberButtonProps {
   children: React.ReactNode;
@@ -11,6 +13,13 @@ interface InviteMemberButtonProps {
 
 export const InviteMemberButton = ({ children, asChild }: InviteMemberButtonProps) => {
   const [open, setOpen] = useState(false);
+  const currentUser = useCurrentUser();
+  if (!currentUser) return null;
+  const { getOrganizationMemberByUserId } = useOrganizationMembersStore();
+  const currentUserMember = getOrganizationMemberByUserId(currentUser.id);
+  if (!currentUserMember || (currentUserMember.role !== "ADMIN" && currentUserMember.role !== "OWNER")) {
+    return null;
+  }
   return (
     <Dialog
       open={open}

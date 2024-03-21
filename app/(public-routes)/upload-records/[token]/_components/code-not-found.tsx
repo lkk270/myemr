fdc2c40843792d6f49d9185d2517e8a8eaafc8a1
Lee Korff-Korn <leekk270@gmail.com>
2024-03-21 16:49:52 +0100

    minor fix

--- app/(public-routes)/upload-records/[token]/_components/code-not-found.tsx
+++ app/(public-routes)/upload-records/[token]/_components/code-not-found.tsx
@@ -6,7 +6,7 @@ export const CodeNotFound = () => {
   return (
     <main className="h-screen w-full flex flex-col justify-center items-center">
       <div className="text-lg xs:text-xl sm:text-4xl font-bold p-3 xs:p-8">Code is not valid</div>
-      <Link href="/" onDragStart={(e) => e.preventDefault()}>
+      <Link href="/">
         <Button variant={"gooeyLeftGhost"} className="bg-secondary shadow-lg border-b-[4px] border-[#8d4fff]">
           <MoveLeft className="w-3 h-3 mr-2" />
           Go Home

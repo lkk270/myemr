7007568a31ca17273d4ea87abb0f71158d68e0d4
Lee Korff-Korn <leekk270@gmail.com>
2024-04-06 00:13:40 +0200

    minor fix to spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/components/loading/spinner";
 const Loading = () => {
   return (
     <>
-      <div className="fixed left-[50%] top-[50%]">
+      <div className="fixed left-[50%] top-[50%] transform -translate-x-1/2 -translate-y-1/2">
         <Spinner size="lg" />
       </div>
     </>

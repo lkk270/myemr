e86bcc136bb4d7b8faf651ba7bc47575da87ba91
Lee Korff-Korn <leekk270@gmail.com>
2024-04-06 19:54:15 +0200

    clean up removed clerk

--- app/(landing)/_components/heading.tsx
+++ app/(landing)/_components/heading.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-// import { useUser } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
-// import { SignUpButton } from "@clerk/clerk-react";
 import Link from "next/link";
 import {
   DropdownMenu,

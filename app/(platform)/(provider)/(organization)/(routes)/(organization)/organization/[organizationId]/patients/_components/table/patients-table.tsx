6de534f218a543f9fbd783c7ea6f60114695b17d
Lee Korff-Korn <leekk270@gmail.com>
2024-04-09 21:47:01 +0200

    mf

--- app/(platform)/(provider)/(organization)/(routes)/(organization)/organization/[organizationId]/patients/_components/table/patients-table.tsx
+++ app/(platform)/(provider)/(organization)/(routes)/(organization)/organization/[organizationId]/patients/_components/table/patients-table.tsx
@@ -52,7 +52,7 @@ export function PatientsTable({ data }: DataTableProps) {
       className={cn(
         data.length <= 6
           ? `min-h-[${53 * data.length}px]`
-          : "min-h-[300px]" && "xs:max-h-[calc(100vh-210px)] max-h-[calc(100vh-285px)] overflow-y-scroll",
+          : "min-h-[300px]" && "xs:max-h-[calc(100vh-205px)] max-h-[calc(100vh-290px)] overflow-y-scroll",
       )}
     />
   );

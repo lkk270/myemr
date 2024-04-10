402522be713a22c47f1338d9dbc35a52573addfd
Lee Korff-Korn <leekk270@gmail.com>
2024-04-10 13:34:47 +0200

    added landing page structure;

--- app/(landing)/_components/heroes.tsx
+++ app/(landing)/_components/heroes.tsx
@@ -2,13 +2,88 @@ import Image from "next/image";
 
 export const Heroes = () => {
   return (
-    <div className="flex flex-col items-center justify-center max-w-5xl">
-      <div className="flex items-center gap-x-10">
-        <div className="relative w-[250px] h-[250px] xxs:w-[300px] xxs:h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
-          <Image draggable={false} src="/files2.svg" fill className="object-contain" alt="Files" />
+    // Change flex direction to column-reverse on small screens, default is row
+    <div className="flex flex-col gap-y-7">
+      <div className="md:gap-x-2 gap-y-4 flex flex-col md:flex-row w-full px-5 items-center justify-between">
+        <div className="flex-1 max-w-full md:max-w-[35%] mt-5 md:mt-0">
+          Let's talk about it. Here's some text that you want to align to the left of the hero section on larger
+          screens, and below the image on smaller screens.
         </div>
-        <div className="relative h-[400px] w-[400px] hidden md:flex">
-          <Image src="/preparation.svg" fill className="object-contain" draggable={false} alt="Preparation" />
+        <div className="flex-1 max-w-full md:max-w-[65%] relative">
+          <Image
+            draggable={false}
+            src="/dark-home.png"
+            layout="responsive"
+            className="rounded-lg shadow-md shadow-secondary hidden dark:block"
+            width={400}
+            height={400}
+            alt="Files"
+          />
+          <Image
+            draggable={false}
+            src="/light-home.png"
+            layout="responsive"
+            className="dark:hidden rounded-lg shadow-md shadow-secondary"
+            width={400}
+            height={400}
+            alt="Files"
+          />
+        </div>
+      </div>
+      <div className="md:gap-x-2 gap-y-4 flex flex-col md:flex-row w-full px-5 items-center justify-between">
+        <div className="md:hidden flex-1 max-w-full md:max-w-[35%] mt-5 md:mt-0">
+          Let's talk about it. Here's some text that you want to align to the left of the hero section on larger
+          screens, and below the image on smaller screens.
+        </div>
+        <div className="flex-1 max-w-full md:max-w-[65%] relative">
+          <Image
+            draggable={false}
+            src="/dark-files.png"
+            layout="responsive"
+            className="rounded-lg shadow-md shadow-secondary hidden dark:block"
+            width={400}
+            height={400}
+            alt="Files"
+          />
+          <Image
+            draggable={false}
+            src="/light-files.png"
+            layout="responsive"
+            className="dark:hidden rounded-lg shadow-md shadow-secondary"
+            width={400}
+            height={400}
+            alt="Files"
+          />
+        </div>
+        <div className="hidden md:flex flex-1 max-w-full md:max-w-[35%] mt-5 md:mt-0">
+          Let's talk about it. Here's some text that you want to align to the left of the hero section on larger
+          screens, and below the image on smaller screens.
+        </div>
+      </div>
+      <div className="md:gap-x-2 gap-y-4 flex flex-col md:flex-row w-full px-5 items-center justify-between">
+        <div className="flex-1 max-w-full md:max-w-[35%] mt-5 md:mt-0">
+          Let's talk about it. Here's some text that you want to align to the left of the hero section on larger
+          screens, and below the image on smaller screens.
+        </div>
+        <div className="flex-1 max-w-full md:max-w-[65%] relative">
+          <Image
+            draggable={false}
+            src="/dark-meds.png"
+            layout="responsive"
+            className="rounded-lg shadow-md shadow-secondary hidden dark:block"
+            width={400}
+            height={400}
+            alt="Files"
+          />
+          <Image
+            draggable={false}
+            src="/light-home.png"
+            layout="responsive"
+            className="dark:hidden rounded-lg shadow-md shadow-secondary"
+            width={400}
+            height={400}
+            alt="Files"
+          />
         </div>
       </div>
     </div>

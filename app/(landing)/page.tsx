be1305b14cd1e453c9bfc8effaa85f72d28e68c9
Lee Korff-Korn <leekk270@gmail.com>
2024-04-10 13:34:47 +0200

    added landing page structure;

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -9,8 +9,8 @@ const MarketingPage = () => {
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#1F1F1F]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#25284a]"></div>
         <Heading />
-        <Heroes />
       </div>
+      <Heroes />
       {/* <Footer /> */}
     </div>
   );

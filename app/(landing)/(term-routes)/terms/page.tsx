e2a1e770904a2e36a2ef6625feca36327f26d579
Lee Korff-Korn <leekk270@gmail.com>
2024-04-11 12:29:16 +0200

    mf

--- app/(landing)/(term-routes)/terms/page.tsx
+++ app/(landing)/(term-routes)/terms/page.tsx
@@ -91,8 +91,8 @@ const TermsPage = () => {
   return (
     <div className="min-h-full flex flex-col xs:pt-8 sm:pt-12">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#1F1F1F]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#25284a]"></div>
+        <div className="bg-[#fbe2e3] fixed top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#1F1F1F]"></div>
+        <div className="bg-[#dbd7fb] fixed top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#25284a]"></div>
         <div className="justify-start flex flex-col items-left text-left overflow-y-scroll max-w-[850px] w-full gap-y-5">
           <div className="flex flex-col gap-y-3">
             <h1 className="text-xl sm:text-3xl font-bold pb-4">Terms & Conditions for MyEMR</h1>
--- app/(public-routes)/auth/layout.tsx
+++ app/(public-routes)/auth/layout.tsx
@@ -7,7 +7,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
         <BasicNavbar />
       </div>
       <div className="flex overflow-auto h-screen">
-        <main className="flex-1 flex pt-6 sm:pt-0 justify-center px-1" style={{ minHeight: "100vh" }}>
+        <main className="flex-1 flex pt-0 justify-center px-1" style={{ minHeight: "100vh" }}>
           <div className="overflow-y-auto max-h-full w-full max-w-[500px]">{children}</div>
         </main>
       </div>

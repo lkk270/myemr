4675c473ca50a5f23ee0c17f93da502f302566c9
Lee Korff-Korn <leekk270@gmail.com>
2024-04-11 02:06:16 +0200

    mf

--- app/(landing)/_components/heroes.tsx
+++ app/(landing)/_components/heroes.tsx
@@ -235,12 +235,10 @@ export function StickyScrollRevealDemo() {
       <div className="overflow-auto h-screen">
         <Navbar scrolled={true} />
         <main className="overflow-auto h-screen pt-16">
-          <div className="min-h-full flex flex-col xs:pt-8 sm:pt-12">
+          <div className="min-h-full flex flex-col pt-4 xs:pt-8 sm:pt-12">
             <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-0 xxs:px-2 pb-10">
-              <div className="pt-8 xs:pt-0">
-                <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#1F1F1F]"></div>
-                <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#25284a]"></div>
-              </div>
+              <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#1F1F1F]">testingabc</div>
+              <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#25284a]"></div>
               <Heading />
               {/* <Heroes /> */}
 

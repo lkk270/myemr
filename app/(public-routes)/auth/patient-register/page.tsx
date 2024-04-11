549bb7ccdb76e6a4fd951c09bee552b8bd33c8f4
Lee Korff-Korn <leekk270@gmail.com>
2024-04-11 02:12:17 +0200

    mf

--- app/(public-routes)/auth/patient-register/page.tsx
+++ app/(public-routes)/auth/patient-register/page.tsx
@@ -1,7 +1,11 @@
 import { RegisterForm } from "@/auth/components/auth/register-form";
 
 const RegisterPage = () => {
-  return <RegisterForm userType="PATIENT" />;
+  return (
+    <div className="pb-28 sm:pb-0">
+      <RegisterForm userType="PATIENT" />
+    </div>
+  );
 };
 
 export default RegisterPage;

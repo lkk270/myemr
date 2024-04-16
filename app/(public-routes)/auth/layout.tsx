fe4087e037e6235f2ae27422fdb647907b8adfd5
Lee Korff-Korn <leekk270@gmail.com>
2024-04-16 20:14:08 +0200

    minor fix

--- app/(public-routes)/auth/layout.tsx
+++ app/(public-routes)/auth/layout.tsx
@@ -6,9 +6,9 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="justify-start p-4">
         <BasicNavbar />
       </div>
-      <div className="flex overflow-auto h-screen">
-        <main className="flex-1 flex pt-0 justify-center px-1" style={{ minHeight: "100vh" }}>
-          <div className="overflow-y-auto max-h-full w-full max-w-[500px]">{children}</div>
+      <div className="flex overflow-auto h-full">
+        <main className="flex-1 flex pt-0 justify-center px-1">
+          <div className="max-h-[87vh] w-full max-w-[500px]">{children}</div>
         </main>
       </div>
     </>

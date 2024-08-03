25d9753f6a10e467b9e5f6cc518087677c328465
Lee Korff-Korn <leekk270@gmail.com>
2024-08-02 22:29:29 -0400

    reload bug on tpa side fixed

--- app/(platform)/(access-patient-with-code)/(non-file-system)/layout.tsx
+++ app/(platform)/(access-patient-with-code)/(non-file-system)/layout.tsx
@@ -1,55 +1,62 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { getAccessPatientCodeByToken } from "@/auth/data";
-// import { MainNavbar } from "@/components/headers/main-navbar";
 import { Navbar } from "../../(patient)/_components/navbar";
 import { logout } from "@/auth/actions/logout";
 
-import { startTransition, useEffect, useState } from "react";
-// import { NewMedicationModal } from "../../(patient)/(non-file-system)/medications/_components/modals/new-medication-modal";
-// import { ViewMedicationModal } from "../../(patient)/(non-file-system)/medications/_components/modals/view-medication-modal";
-
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [shouldReload, setShouldReload] = useState(false);
   const sessionObj = useSession();
   const session = sessionObj.data;
-  const [isMounted, setIsMounted] = useState(false);
-
-  // if (!session) {
-  //   window.location.reload();
-  // }
 
   useEffect(() => {
-    const now = new Date();
-    if (session && new Date(session.expires) < now) {
-      logout();
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    // Only check session status if the component is mounted to prevent server-side checks
+    if (isMounted) {
+      if (!session) {
+        if (!shouldReload) {
+          setShouldReload(true); // Set the reload flag on first check
+        } else {
+          window.location.reload(); // Reload only if flag is already set, preventing continuous loop
+        }
+      } else {
+        setShouldReload(false); // Reset reload flag if session exists
+      }
+
+      // Check if session is expired and log out if it is
+      const now = new Date();
+      if (session && new Date(session.expires) < now) {
+        logout();
+      }
     }
-  }, [session]);
+  }, [session, isMounted]);
 
   useEffect(() => {
-    const checkValidCode = () => {
-      startTransition(() => {
-        getAccessPatientCodeByToken(session?.tempToken).then((data) => {
-          if (!data) {
-            logout();
-          } else {
-            setIsMounted(true);
-          }
-        });
-      });
-    };
-    checkValidCode();
-  }, [session?.tempToken]);
+    if (isMounted && session?.tempToken) {
+      const checkValidCode = async () => {
+        const data = await getAccessPatientCodeByToken(session.tempToken);
+        if (!data) {
+          logout();
+        }
+      };
 
-  if (!isMounted) {
-    return null;
+      checkValidCode();
+    }
+  }, [session?.tempToken, isMounted]);
+
+  if (!isMounted || shouldReload) {
+    return null; // Prevent rendering while waiting for reload or session check
   }
+
   return (
     <div className="flex overflow-auto h-[95vh] xs:h-screen">
       <Navbar tempAccess={true} />
-      {/* <NewMedicationModal />
-      <ViewMedicationModal /> */}
       <main className="pt-16 flex-1 overflow-y-auto">{children}</main>
     </div>
   );

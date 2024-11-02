21ed587d15583158016cd2d01f12720d8a4dd6ba
Lee Korff-Korn <leekk270@gmail.com>
2024-11-02 14:25:01 -0600

    setup completeeeee

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,13 @@
-module.exports = {
-    testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: ['src/**/*.js'],
-    coverageReporting: {
-      reporters: ['text', 'lcov']
-    },
-    preset: 'ts-jest',
-  }; 
+// jest.config.ts
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  verbose: true,
+  coverageReporters: ["text", "lcov"], // Correct option
+};
+
+export default config;
